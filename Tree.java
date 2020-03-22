01b68a788f8d80111fde2492f3b7d20d8fb9b2cd
Michael Adair <mjadair91@gmail.com>
2020-03-22 15:51:21 +0000

    Printed a tree

--- Tree.java
+++ Tree.java
@@ -1,12 +1,19 @@
 public class Tree {
 
-public static main void(String[] args) {
+public static void main (String[] args) {
 
 //Introduce and plant a tree
 
 System.out.println("This is my first message printed in Java");
 
 System.out.println("This project should show a planted tree...");
+
+
+System.out.println("  *  ");
+System.out.println(" *** ");
+System.out.println(" *** ");
+System.out.println("  *  ");
+System.out.println("  *  ");  
 
 }
 
