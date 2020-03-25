d5dd3d8de438e37ca0da661e274f6436e07807e0
Michael Adair <mjadair91@gmail.com>
2020-03-25 17:30:43 +0000

    added string

--- Droid.java
+++ Droid.java
@@ -12,13 +12,18 @@ batteryLevel = 100;
  }
 
 
+ public String toString() {
+   return "Ahoy there, my name is " + name; 
+ }
+
+
 
   public static void main(String[] args) {
 
     Droid marvin = new Droid("Marvin");
 
 
-    System.out.println(marvin.name);
+    System.out.println(marvin);
 
 
   }
