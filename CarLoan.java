d95e2f98b5ba7ed4a846f6277aab646f6178b3b4
Michael Adair <mjadair91@gmail.com>
2020-04-06 17:09:09 +0100

    recompiled

--- CarLoan.java
+++ CarLoan.java
@@ -4,7 +4,7 @@ public class CarLoan {
 
 	public static void main(String[] args) {
 
-    int carLoan = 1000;
+    int carLoan = 10000;
     int loanLength = 3;
     int interestRate = 5;
     int downPayment = 2000;
@@ -14,7 +14,7 @@ public class CarLoan {
 if (loanLength <= 0 || interestRate <= 0) {
   System.out.println("Error! You must take out a valid loan.");
 }
-else if(downPayment <= carLoan) {
+else if(downPayment >= carLoan) {
   System.out.println("The car can be paid in full");
 
 }
