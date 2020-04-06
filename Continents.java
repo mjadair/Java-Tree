a5d50ec9b9a363013417f1a66c3b85d97501c2ea
Michael Adair <mjadair91@gmail.com>
2020-04-06 17:14:23 +0100

    added switch statement

--- Continents.java
+++ Continents.java
@@ -1,7 +1,14 @@
 public class Continents {
 	public static void main(String[] args) {
 
-		
+    int continent = 4;
+    
+switch(continent){
+  case 1:
+  System.out.println("North America: Mexico City, Mexico");
+
+}
+
 
 	}
 }
