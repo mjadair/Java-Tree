bf2927929647f682d5e9656b07a86ba71c6dff62
Michael Adair <mjadair91@gmail.com>
2020-04-06 16:53:32 +0100

    added variables

---
+++ CarLoan.java
@@ -0,0 +1,24 @@
+public class CarLoan {
+
+  int carLoan;
+  int loanLength;
+  int interestRate;
+  int downPayment;
+
+
+
+  public CarLoan() {
+
+    carLoan = 1000;
+    loanLength = 3;
+    interestRate = 5;
+    downPayment = 2000;
+    
+     }
+
+	public static void main(String[] args) {
+
+	
+
+	}
+}
