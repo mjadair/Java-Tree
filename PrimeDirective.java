cf98b607110d741c6663f79a7f7ed60bcd4127f5
Michael Adair <mjadair91@gmail.com>
2020-04-27 12:31:29 +0100

    added multiple prime numbers checker

--- PrimeDirective.java
+++ PrimeDirective.java
@@ -6,36 +6,57 @@ class PrimeDirective {
 
   public boolean isPrime(int number) {
 
-
-  //If statement for edge cases
+    // If statement for edge cases
     if (number == 2) {
       return true;
     } else if (number < 2) {
       return false;
     }
 
-  //for loop that iterates through numbers to check if numbers before are not prime
+    // for loop that iterates through numbers to check if numbers before are not
+    // prime
     else {
       for (int i = 2; i < number; i++) {
         if (number % i == 0) {
           return false;
         }
       }
-    //otherwise true
+      // otherwise true
       return true;
     }
   }
+
+  // a method that returns an array list of numbers
+  public ArrayList<Integer> onlyPrimes(int[] numbers) {
+
+
+    //an empty array list of for prime numbers
+  ArrayList<Integer> primes = new ArrayList<Integer>();
+
+
+for (int number : numbers) {
+ if (isPrime(number)) {
+   primes.add(number);
+ }
+
+  }
+
+  return primes;
+}
 
   public static void main(String[] args) {
 
     PrimeDirective pd = new PrimeDirective();
     int[] numbers = { 6, 29, 28, 33, 11, 100, 101, 43, 89 };
 
-
     System.out.println(pd.isPrime(0));
     System.out.println(pd.isPrime(2));
     System.out.println(pd.isPrime(7));
     System.out.println(pd.isPrime(28));
+
+
+
+    System.out.println(pd.onlyPrimes(numbers));
 
   }
 
