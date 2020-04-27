0254fe150664eb87b84e20e282b0de4850eddd5d
Michael Adair <mjadair91@gmail.com>
2020-04-27 12:58:03 +0100

    added DNA app

---
+++ DNA.java
@@ -0,0 +1,28 @@
+public class DNA {
+
+
+  //finding valid DNA structures in order to practice JAVA string methods
+
+  public static void main(String[] args) {
+
+    //  -. .-.   .-. .-.   .
+    //    \   \ /   \   \ / 
+    //   / \   \   / \   \  
+    //  ~   `-~ `-`   `-~ `-
+
+
+    String dna1 = "ATGCGATACGCTTGA";
+    String dna2 = "ATGCGATACGTGA";
+    String dna3 = "ATTAATATGTACTGA";
+
+
+String dna = dna1;
+
+System.out.println(dna.length());
+
+
+
+
+  }
+
+}
